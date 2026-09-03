a9f02d174af1d24d37e49d46de1b2737b0672a4d
AishaPenmesta <aisha.penmesta@gmail.com>
2026-09-03 19:40:09 +0000

    update

--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		// the command window when you compile and run this program
+		// 
+		String birthday = new String("Happy Birthday to ");
+		String bob = new String("Bob!");
+		String you = new String("you!");
+		System.out.println(birthday + you);
+		System.out.println(birthday + you);
+		System.out.println(birthday + bob);
+		System.out.print(birthday + you);
+
 	}
 }
