e61d52356189deb991a22bcf9cd018ca2bcfd714
AishaPenmesta <aisha.penmesta@gmail.com>
2026-09-22 19:26:15 +0000

    update

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -9,6 +9,18 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("The first variable is: 5");
+		int x = 5;
+		System.out.println("The second variable is: 12");
+		int y = 12;
+		boolean same = x == y;
+		if(same) {
+			System.out.println("The values are the same!");
+		}
+		boolean different = x != y;
+		if(different){
+			System.out.println("The values are different!");
+		}
+		
 	}
 }
--- 000_Labs/012_if_statements_more/base_code/starter.java
+++ 000_Labs/012_if_statements_more/base_code/starter.java
@@ -9,6 +9,21 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		//copied it from lab 11 because i thought it was asking to use scanner there
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Type an integer value: ");
+		int x = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Type another integer value: ");
+		int y = sc.nextInt();
+		sc.nextLine();
+		boolean same = x == y;
+		if(same) {
+			System.out.println("The values are the same!");
+		}
+		boolean different = x != y;
+		if(different){
+			System.out.println("The values are different!");
+		} 
 	}
 }
